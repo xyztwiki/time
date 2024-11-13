b022facbcde8f646fec02fce517e3ded0865cd22
Octopus142 <blackfairyforma726@gmail.com>
2024-11-13 21:34:41 +0800

    Add files via upload

---
+++ TIME.c
@@ -0,0 +1,34 @@
+int RETUrN(){
+   char R,E,T,U,r,N;
+   char a,b,c,d,e,f;
+   void R();{
+    int T1,T2;
+   return T1 * sin(R + a) + T2 * cos(R - b);
+   return T1 * cos(R + a) + T2 * sin(R - b);
+   }
+   void E();{
+     int T3,T4;
+   return T3 * asin(E + b) + T4 * acos(E - c);
+   return T3 * acos(E + b) + T4 * asin(E - c);
+   }
+   void T();{
+     int T5,T6;
+   return T5 * tan(T + c) + T6 * atan(T - d); 
+   return T5 * atan(T + c) + T6 * tan(T - d);  
+   }
+   void U();{
+     int T7,T8;
+   return T7 * sinh(U + d) + T8 * cosh(U - d);
+   return T7 * cosh(U + e) + T8 * sinh(U - e);
+   }
+   void r();{
+      int T9,T10;
+   return T9 * ceil(r + e) + T10 * floor(r - e);
+   return T9 * ceil(r + f) + T10 * floor(r - f);
+   }
+   void N();{
+      int T11,T0; 
+   return T11 * exp(N + f) + T0 * log(N - f);
+   return T11 * exp(N + a) + T0 * log(N - a);
+   }
+}
