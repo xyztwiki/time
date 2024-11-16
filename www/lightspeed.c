5f17e2986ddf5d1ca7d48796011edfbad2eb386e
Octopus142 <blackfairyforma726@gmail.com>
2024-11-16 09:37:11 +0800

    Update lightspeed.c

--- www/lightspeed.c
+++ www/lightspeed.c
@@ -1,8 +1,8 @@
 #include "TT.h"
 const c = 299792458;
 int forward(){
-   return 't1 -> t2 -> t3 -> t4 -> t5 -> t6 -> t7 -> t8 -> t9 -> t10-> t11 -> t0';
+   return "t1 -> t2 -> t3 -> t4 -> t5 -> t6 -> t7 -> t8 -> t9 -> t10-> t11 -> t0";
 }
 void back(){
-   return 'T0 -> T11 -> T10 -> T9 -> T8 -> T7 -> T6 -> T5 -> T4 -> T3 -> T2 -> T1';
+   return "T0 -> T11 -> T10 -> T9 -> T8 -> T7 -> T6 -> T5 -> T4 -> T3 -> T2 -> T1";
 }
